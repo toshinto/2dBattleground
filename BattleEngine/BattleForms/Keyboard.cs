22eaa49b5e060a0e876248aaa661d5c8c73bd76a
Ix <ixtreon@gmail.com>
2016-02-10 12:46:05 +0200

    Added a keyboard class for better controlz n shit.

---
+++ BattleEngine/BattleForms/Keyboard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattleForms
+{
+    class Keyboard
+    {
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetKeyboardState(byte[] lpKeyState);
+
+
+        static bool[] keyStates;
+
+        public static void Update()
+        {
+            byte[] keys = new byte[256];
+
+            //Get pressed keys
+            if (!GetKeyboardState(keys))
+                throw new Exception("GetKeyboardState failed!");
+
+            keyStates = keys
+                .Select(key => (key & 0x80) != 0)
+                .ToArray();
+
+        }
+
+        public static bool GetKeyState(Keys k)
+        {
+            return keyStates[(int)k];
+        }
+    }
+}
